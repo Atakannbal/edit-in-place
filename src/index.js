b9af9d2a97293f8d9ebbfd52a1d21e0e21692d30
Atakan Bal <atakan.bal@kns.com.tr>
2021-11-21 10:33:18 +0300

    refactor: esmodules and duck typing

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Interface } from './utils/interface.js'
+export { default as EditInPlaceArea } from './utils/edit-in-place-area'
+export { default as EditInPlaceField } from './utils/edit-in-place-field'
